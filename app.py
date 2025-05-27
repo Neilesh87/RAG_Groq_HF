3f03f16da05834692fb94c11cc25a7b5e2457db9
Neilesh87 <nilesh.fca2020@gmail.com>
2025-05-27 16:51:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ llm=ChatGroq(groq_api_key=groq_api_key,model_name="Llama3-8b-8192")
 prompt=ChatPromptTemplate.from_template(
     """
     Answer the questions based on the provided context only.
-    Please provide the most accurate respone based on the question in bullet points but as a separate line 
+    Please provide the most accurate respone based on the question 
     <context>
     {context}
     <context>
